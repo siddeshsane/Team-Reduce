61c93a040e9f9027866dca7c8ed6d1ffd73108da
siddeshsane <65365843+siddeshsane@users.noreply.github.com>
2023-01-16 21:37:33 +0530

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -6,14 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor() {}
-  baseplayers: BasePlayer[] = [
-    { name: 'siddesh', team: 'Team-A', role: 'BL' },
-    { name: 'siddesh', team: 'Team-A', role: 'BL' },
-  ];
+  baseplayers: BasePlayer[] = [];
+  baseCount: Number = 6;
+  tempPlayer: BasePlayer = { name: 'siddesh', team: 'Team-A', role: 'BL' };
 
+  constructor() {}
+
+  ngOnInit() {
+    this.initilize();
+  }
+
+  // initailizes the app
+  private initilize() {
+    for (let i = 0; i < this.baseCount; i++) {
+      this.baseplayers.push(this.tempPlayer);
+    }
+  }
+
+  // when players are added
   public baseplayeradded() {
-    // console.log(this.baseplayers[1].role);
+    console.log(this.baseplayers[1].role);
   }
 }
 export interface BasePlayer {
