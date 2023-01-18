2e203caec281be3ca4e74f34dc54682ab0239f41
siddeshsane <65365843+siddeshsane@users.noreply.github.com>
2023-01-18 12:15:42 +0530

    2 files modified

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -9,6 +9,29 @@ export class StorageService {
   BasePlayer: Player[] = [];
   RotationalPlayers: Player[] = [];
   CombinationPlayers: Player[] = [];
+
+  generatecombinatations(total: number, selection: number, player: Player[]) {
+    var valuesArray = ['apple', 'banana', 'lemon', 'mango', 'lichi'];
+    var combi = [];
+    var temp = [];
+    var slent = Math.pow(2, valuesArray.length);
+
+    for (var i = 0; i < slent; i++) {
+      temp = [];
+      for (var j = 0; j < valuesArray.length; j++) {
+        if (i & Math.pow(2, j)) {
+          temp.push(valuesArray[j]);
+        }
+      }
+      if (temp.length > 0) {
+        combi.push(temp);
+      }
+    }
+
+    combi.sort((a, b) => a.length - b.length);
+    console.log(combi.join('\n'));
+    //return combi;
+  }
 }
 export interface Team {
   id: number;
